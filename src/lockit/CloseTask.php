2b2c2ffc8c2f9dbe45c111e0368e3327314d65a0
jazzwhistle <jdcraftmcpe@gmail.com>
2016-06-13 04:03:05 +0200

    First Commit - DOOR ORIENTATION BROKEN

---
+++ src/lockit/CloseTask.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace LockIt;
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\math\Vector3;
+use pocketmine\item\Item;
+use pocketmine\tile\Chest;
+use pocketmine\level\Level;
+use pocketmine\block\Block;
+
+class CloseTask extends PluginTask {
+
+    private $plugin;
+    private $block;
+    private $top;
+
+        public function __construct(Main $plugin, $block, $top) {
+        parent::__construct($plugin);
+
+        $this->plugin = $plugin;
+        $this->block = $block;
+        $this->top = $top;
+    }
+    
+    public function onRun($tick) {
+    
+        //$this->block->getLevel()->setBlock($this->block, Block::get(Item::IRON_DOOR_BLOCK));
+        if (!$this->top) {
+        $this->block->getLevel()->setBlock(new Vector3($this->block->getX(), $this->block->getY(), $this->block->getZ()), Block::get(Item::IRON_DOOR_BLOCK));
+        
+        }else{
+         $this->block->getLevel()->setBlock(new Vector3($this->block->getX(), ($this->block->getY() - 1), $this->block->getZ()), Block::get(Item::IRON_DOOR_BLOCK));
+           
+        }
+        
+        }
+}
---
+++ src/lockit/Main.php
@@ -0,0 +1,233 @@
+<?php
+
+namespace LockIt;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\math\Vector3;
+use pocketmine\utils\TextFormat;
+use pocketmine\item\Item;
+use pocketmine\item\ItemBlock;
+use pocketmine\block\Block;
+use pocketmine\event\block\BlockBreakEvent;
+
+class Main extends PluginBase implements CommandExecutor, Listener {
+
+    private $session;
+    public $prefs;
+    public $locked;
+    public $treasure;
+
+    public function onEnable() {
+        if (!file_exists($this->getDataFolder())) {
+            mkdir($this->getDataFolder());
+        }
+
+        $this->session = [];
+
+        $this->prefs = new Config($this->getDataFolder() . "prefs.yml", CONFIG::YAML, array(
+            "TakeKey" => true,
+            "AutoClose" => true,
+            "Delay" => 5
+        ));
+
+        $lockedYml = new Config($this->getDataFolder() . "locked.yml", Config::YAML, array());
+        $this->locked = $lockedYml->getAll();
+
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+        if ($sender instanceof Player) {
+
+            if (!isset($args[0])) return false;
+            
+            switch ($args[0]) {
+
+                case "off":
+                case "stop":
+                    unset($this->session[$sender->getPlayer()->getName()]);
+                    $sender->sendMessage(TEXTFORMAT::RED . "LockIt Tap Mode : OFF");
+
+                    return true;
+                //more commands here...
+
+                default:
+                    break;
+            }
+
+
+
+            if (!is_numeric($args[0])) {
+                $sender->sendMessage("Invalid ID number");
+                return false;
+            }
+            $block = Item::get($args[0]);
+            if (!$block instanceof Item) {
+                $sender->sendMessage("Invalid ID");
+                return false;
+            }
+
+            $this->session[$sender->getName()] = $args[0];
+            $sender->sendMessage(TEXTFORMAT::GREEN . "LockIt Tap Mode : ON");
+            $sender->sendMessage(TEXTFORMAT::YELLOW . "Touch the BOTTOM of doors to lock them");
+            $sender->sendMessage(TEXTFORMAT::YELLOW . "Type " . TEXTFORMAT::RED . "/lockit off" . TEXTFORMAT::YELLOW . " to stop");
+        } else {
+            $sender->sendMessage(TEXTFORMAT::RED . "Please run the command in the game");
+        }
+        return true;
+    }
+
+    public function onPlayerInteract(PlayerInteractEvent $event) {
+        
+        if (!($event->getBlock()->getId() === 71 )) {
+            return;
+        }
+
+        if (isset($this->session[$event->getPlayer()->getName()])) {
+
+            // if it's someone who is locking doors...
+
+            $block = $event->getBlock();
+            $keyid = $this->session[$event->getPlayer()->getName()];
+
+            $this->locked[$block->getX() . ":" . $block->getY() . ":" . $block->getZ() . ":" . $block->getLevel()->getFolderName()] = array(
+                "keyid" => $keyid,
+                "owner" => $event->getPlayer()->getName()
+            );
+
+            $this->saveLocked();
+
+            $event->getPlayer()->sendMessage(TEXTFORMAT::GREEN . "Door locked. Key: $keyid");
+            $event->setCancelled(true);
+            return true;
+        } else {
+            
+            // regular player taps a door, check above and below
+            //Check Item in Hand INVENTORY
+            
+            $block = $event->getBlock();
+            $inv = $event->getPlayer()->getInventory();
+            $inhand = $inv->getItemInHand();
+            
+            //CHECK THE BLOCK ITSELF
+            
+                if (isset($this->locked[$event->getBlock()->x . ":" . $event->getBlock()->y . ":" . $event->getBlock()->z . ":" . $event->getPlayer()->getLevel()->getName()])) {
+                //it's a locked door
+
+                $locked = $this->locked[$event->getBlock()->x . ":" . $event->getBlock()->y . ":" . $event->getBlock()->z . ":" . $event->getPlayer()->getLevel()->getName()];
+                $keyid = $locked["keyid"];
+
+                if ($inhand->getId() == $keyid) {
+                    if($this->prefs->get("TakeKey")){
+                    --$inhand->count;
+                    $inv->setItemInHand($inhand);  
+                    } 
+                    
+                    //open door for DELAY seconds
+                    
+                    if($this->prefs->get("AutoClose")){
+                        
+                    $task = new CloseTask($this, $block, false);
+                    $taskid = $this->getServer()->getScheduler()->scheduleDelayedTask($task, 20 * $this->prefs->get("Delay"));
+                    $task->setHandler($taskid);
+                    }
+                    
+                    return;
+                    
+                } else {
+                    $event->getPlayer()->sendMessage(TextFormat::RED . "You don't have the key ");
+                    $event->setCancelled(true);
+                }
+            }
+            
+            //CHECK THE BLOCK BELOW
+            
+                if (isset($this->locked[$event->getBlock()->x . ":" . ($event->getBlock()->y - 1) . ":" . $event->getBlock()->z . ":" . $event->getPlayer()->getLevel()->getName()])) {
+                //it's a locked door
+
+                $locked = $this->locked[$event->getBlock()->x . ":" . ($event->getBlock()->y - 1) . ":" . $event->getBlock()->z . ":" . $event->getPlayer()->getLevel()->getName()];
+                $keyid = $locked["keyid"];
+
+                if ($inhand->getId() == $keyid) {
+                    if($this->prefs->get("TakeKey")){
+                    --$inhand->count;
+                    $inv->setItemInHand($inhand);  
+                    } 
+                    
+                    //open door for DELAY seconds
+                    
+                    if($this->prefs->get("AutoClose")){
+                        
+                    $task = new CloseTask($this, $block, true);
+                    $taskid = $this->getServer()->getScheduler()->scheduleDelayedTask($task, 20 * $this->prefs->get("Delay"));
+                    $task->setHandler($taskid);
+                    }
+                    
+                    return;
+                    
+                } else {
+                    $event->getPlayer()->sendMessage(TextFormat::RED . "You don't have the key ");
+                    $event->setCancelled(true);
+                }
+            }
+            
+
+        }
+    }
+
+    public function onBlockBreak(BlockBreakEvent $event) {
+        $block = $event->getBlock();
+
+        if (!($block->getID() === 71 ))
+            return;
+
+        if (isset($this->locked[$block->getX() . ":" . $block->getY() . ":" . $block->getZ() . ":" . $block->getLevel()->getFolderName()])) {
+            $player = $event->getPlayer();
+            $locked = $this->locked[$block->getX() . ":" . $block->getY() . ":" . $block->getZ() . ":" . $block->getLevel()->getFolderName()];
+
+            $owner = $locked["owner"];
+
+            if (!($player->hasPermission("lockit.admin") || (strtolower($owner) === strtolower($player->getName())))) {
+                $player->sendMessage("You do not have permission to break this door. Owner: " . $player->getName());
+                $event->setCancelled(true);
+                return;
+            }
+
+            //Delete this locked door
+            unset($this->locked[$block->getX() . ":" . $block->getY() . ":" . $block->getZ() . ":" . $block->getLevel()->getFolderName()]);
+            $this->saveLocked();
+        }
+
+        if (isset($this->locked[$block->getX() . ":" . ($block->getY() - 1) . ":" . $block->getZ() . ":" . $block->getLevel()->getFolderName()])) {
+            $player = $event->getPlayer();
+            $locked = $this->locked[$block->getX() . ":" . ($block->getY() - 1) . ":" . $block->getZ() . ":" . $block->getLevel()->getFolderName()];
+
+            $owner = $locked["owner"];
+
+            if (!($player->hasPermission("lockit.admin") || (strtolower($owner) === strtolower($player->getName())))) {
+                $player->sendMessage("You do not have permission to break this door. Owner: " . $player->getNmae());
+                $event->setCancelled(true);
+                return;
+            }
+
+            //Delete this locked door
+            unset($this->locked[$block->getX() . ":" . ($block->getY() - 1) . ":" . $block->getZ() . ":" . $block->getLevel()->getFolderName()]);
+            $this->saveLocked();
+        }
+    }
+
+    function saveLocked() {
+        $lockedYml = new Config($this->getDataFolder() . "locked.yml", Config::YAML);
+        $lockedYml->setAll($this->locked);
+        $lockedYml->save();
+    }
+
+}
